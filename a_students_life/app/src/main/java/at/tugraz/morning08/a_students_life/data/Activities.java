700274c3d172c9763ab7046ed1eee336f2df21a2
Florian Karner <florian.karner@student.tugraz.at>
2018-04-11 11:52:10 +0200

    [FK, JG] Activity WatchTV

--- a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/data/Activities.java
+++ a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/data/Activities.java
@@ -3,9 +3,17 @@ package at.tugraz.morning08.a_students_life.data;
 
 public final class Activities
 {
+    //Main Money
     public static void askForMoney(Student student) {
         student.addTimeUnits(2); // !!! Has to be made first at every Activities!!!
 
         student.addCash(100);
+    }
+
+    //Main Stress
+    public static void watchTV(Student student) {
+        student.addTimeUnits(2); // !!! Has to be made first at every Activities!!!
+
+        student.getStats().increaseStress(12);
     }
 }
