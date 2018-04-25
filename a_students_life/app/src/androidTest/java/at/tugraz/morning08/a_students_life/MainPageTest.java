f0abf85dddb6c8284f3567839cb0dcf922cddf1a
Alessa Angerschmid <alessa.angerschmid@gmail.com>
2018-04-25 08:06:29 +0200

    [AA, JG] Added UI Test for lose conditions

--- a_students_life/app/src/androidTest/java/at/tugraz/morning08/a_students_life/MainPageTest.java
+++ a_students_life/app/src/androidTest/java/at/tugraz/morning08/a_students_life/MainPageTest.java
@@ -12,6 +12,10 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+import at.tugraz.morning08.a_students_life.data.Student;
 
 
 
@@ -34,6 +38,23 @@ public class MainPageTest {
         Espresso.onView(withId(R.id.mainPage)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void openLoseConditionDialogTest()
+    {
+        Student.getInstance().getStats().setHunger(5);
+        Espresso.onView(withId(R.id.energy)).perform(click());
+        Espresso.onView(withId(R.id.sleep_button)).perform(click());
+        Espresso.onView(withId(R.id.popUp_eneregy_ll)).check(matches(isDisplayed()));
+        Espresso.onView(withId(R.id.mainPage)).check(matches(isDisplayed()));
+        assertEquals(0, Student.getInstance().getStats().getHunger());
+        Espresso.onView(withText("Game over!")).check(matches(isDisplayed()));
+    }
 
+    @Test
+    public void calendarBackButtonTest() throws Exception {
+        Espresso.onView(withId(R.id.btnCalendar)).perform(click());
+        Espresso.pressBack();
+        Espresso.onView(withId(R.id.mainPage)).check(matches(isDisplayed()));
+    }
 
 }
