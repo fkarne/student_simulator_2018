d563b7f6b700f625f73c408152def657b3acdfb0
Alessa Angerschmid <alessa.angerschmid@gmail.com>
2018-04-30 23:13:14 +0200

    [AA, JG] added german language file; added option to choose language & created UI Test

---
+++ a_students_life/app/src/androidTest/java/at/tugraz/morning08/a_students_life/StartMenuTest.java
@@ -0,0 +1,57 @@
+package at.tugraz.morning08.a_students_life;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import at.tugraz.morning08.a_students_life.data.Student;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
+
+public class StartMenuTest {
+    @Rule
+    public ActivityTestRule<StartMenuActivity> mRule = new ActivityTestRule<>(StartMenuActivity.class);
+
+    @Test
+    public void changeLanguageToGermanTest() throws Exception {
+        Espresso.onView(withId(R.id.options_btn)).perform(click());
+        Espresso.onView(withId(R.id.lang_de_rb)).perform(click());
+        Espresso.onView(withId(R.id.save_lang_btn)).perform(click());
+        Espresso.onView(withId(R.id.options_btn)).check(matches(withText("Optionen")));
+    }
+
+
+    @Test
+    public void changeLanguageTwiceTest() throws Exception {
+        // change to german
+        Espresso.onView(withId(R.id.options_btn)).perform(click());
+        Espresso.onView(withId(R.id.lang_de_rb)).perform(click());
+        Espresso.onView(withId(R.id.save_lang_btn)).perform(click());
+
+        //change back to english
+        Espresso.onView(withId(R.id.options_btn)).perform(click());
+        Espresso.onView(withId(R.id.lang_en_rb)).perform(click());
+        Espresso.onView(withId(R.id.save_lang_btn)).perform(click());
+        Espresso.onView(withId(R.id.options_btn)).check(matches(withText("Options")));
+    }
+
+    @Test
+    public void cancelOptionsTest() throws Exception {
+        Espresso.onView(withId(R.id.options_btn)).perform(click());
+        Espresso.onView(withId(R.id.lang_de_rb)).perform(click());
+        Espresso.onView(withId(R.id.cancel_lang_btn)).perform(click());
+        Espresso.onView(withId(R.id.options_btn)).check(matches(withText("Options")));
+    }
+}
--- a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/StartMenuActivity.java
+++ a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/StartMenuActivity.java
@@ -2,10 +2,18 @@ package at.tugraz.morning08.a_students_life;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.app.Activity;
+import android.widget.RadioButton;
+
+import java.util.Locale;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -31,4 +39,65 @@ public class StartMenuActivity extends AppCompatActivity {
         StartMenuActivity.this.finish();
     }
 
+    /*
+     * Sets chosen language
+     * needs String -> 'en', 'de'
+     */
+    public void setLocale(String lang) {
+        Locale myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, StartMenuActivity.class);
+        startActivity(refresh);
+        finish();
+    }
+
+    // call options file
+    public void show_options(View view) {
+        setContentView(R.layout.activity_start_menu_options);
+    }
+
+
+    public void save_options_language(View view) {
+        RadioButton rdb_en = findViewById(R.id.lang_en_rb);
+        RadioButton rdb_de = findViewById(R.id.lang_de_rb);
+
+        if(rdb_en.isChecked()){
+            setLocale((String) getText(R.string.lang_en_short));
+        }
+        else if(rdb_de.isChecked()){
+            setLocale((String) getText(R.string.lang_de_short));
+        }
+    }
+
+    //reset xml
+    public void cancel_options_language(View view) {
+        setContentView(R.layout.activity_start_menu);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        RadioButton rb_en = findViewById(R.id.lang_en_rb);
+        RadioButton rb_de = findViewById(R.id.lang_de_rb);
+
+        boolean  checked = ((RadioButton) view).isChecked();
+
+        switch(view.getId()) {
+            case R.id.lang_en_rb:
+                if (checked)
+                    rb_en.setTypeface(null, Typeface.BOLD_ITALIC);
+                    //set the other two radio buttons text style to default
+                    rb_de.setTypeface(null, Typeface.NORMAL);
+                break;
+
+            case R.id.lang_de_rb:
+                if (checked)
+                    rb_de.setTypeface(null, Typeface.BOLD_ITALIC);
+                    //set the other two radio buttons text style to default
+                    rb_en.setTypeface(null, Typeface.NORMAL);
+                break;
+        }
+    }
 }
