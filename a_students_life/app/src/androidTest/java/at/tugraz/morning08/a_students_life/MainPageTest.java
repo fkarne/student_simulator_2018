1b3a059404fb214c9c194d5a45255390fd94e3b7
Florian Karner <florian.karner@student.tugraz.at>
2018-05-07 12:55:56 +0200

    [FK, JK] Keyboardvisibility at Setup

--- a_students_life/app/src/androidTest/java/at/tugraz/morning08/a_students_life/MainPageTest.java
+++ a_students_life/app/src/androidTest/java/at/tugraz/morning08/a_students_life/MainPageTest.java
@@ -1,13 +1,12 @@
 package at.tugraz.morning08.a_students_life;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import android.support.test.espresso.Espresso;
 
 import at.tugraz.morning08.a_students_life.data.Student;
 
@@ -16,10 +15,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.junit.Assert.assertEquals;
-
-import at.tugraz.morning08.a_students_life.data.Student;
-
 import static org.junit.Assert.assertEquals;
 
 
@@ -33,13 +28,6 @@ public class MainPageTest {
     public void goToStatsOverviewTest() throws Exception {
         Espresso.onView(withId(R.id.stats)).perform(click());
         Espresso.onView(withId(R.id.statsPage)).check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void goBackToMainPageTest() throws Exception {
-        Espresso.onView(withId(R.id.stats)).perform(click());
-        Espresso.onView(withId(R.id.backButton)).perform(click());
-        Espresso.onView(withId(R.id.mainPage)).check(matches(isDisplayed()));
     }
 
     @Test
