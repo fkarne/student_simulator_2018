e864b2e324e75764154d673d5819d96ddbe8aeb8
Laura <laura.roessl@gmail.com>
2018-05-23 10:00:30 +0200

    [CS, LR] change text sizes, text styles, background, main page design, ...

--- a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/MainPageActivity.java
+++ a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/MainPageActivity.java
@@ -90,7 +90,9 @@ public class MainPageActivity extends AppCompatActivity
         popup(view);
     }
 
-    public void stress_popup(View view) {popup(view); }
+    public void stress_popup(View view) {
+        popup(view);
+    }
 
     public void hunger_popup(View view) {
         popup(view);
@@ -109,6 +111,10 @@ public class MainPageActivity extends AppCompatActivity
     }
 
     public void popup(View view) {
+        //TODO [CS, LR]
+        ImageView student_pic = findViewById(R.id.student_pic);
+        student_pic.setVisibility(View.INVISIBLE);
+
         List<ButtonInfo> activity_list = new ArrayList<>();
         RecyclerView recycler_view;
         PopupAdapter popup_adapter;
