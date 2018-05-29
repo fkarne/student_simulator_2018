ca8da5903123ae7e3cde803da3e5a1ebf7feba1e
Thorsten Bandel <thorsten.bandel@gmail.com>
2018-05-29 12:57:29 +0200

    [TB,FK] changed text to image

--- a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/PopupAdapter.java
+++ a_students_life/app/src/main/java/at/tugraz/morning08/a_students_life/PopupAdapter.java
@@ -1,10 +1,12 @@
 package at.tugraz.morning08.a_students_life;
 
+import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -36,43 +38,43 @@ public class PopupAdapter extends RecyclerView.Adapter<PopupAdapter.PopupViewHol
         switch (activity_list.get(position).getActivity())
         {
             case SLEEP:
-                holder.info.setText("SLEEP");
+                holder.info.setImageDrawable(null);
                 break;
             case NAP:
-                holder.info.setText("NAP");
+                holder.info.setImageDrawable(null);
                 break;
             case EAT:
-                holder.info.setText("EAT");
+                holder.info.setImageDrawable(view.getContext().getDrawable(R.drawable.hunger_btn));
                 break;
             case GOINGOUTTOEAT:
-                holder.info.setText("GOINGOUTTOEAT");
+                holder.info.setImageDrawable(null);
                 break;
             case SNACK:
-                holder.info.setText("SNACK");
+                holder.info.setImageDrawable(null);
                 break;
             case ASKFORMONEY:
-                holder.info.setText("ASKFORMONEY");
+                holder.info.setImageDrawable(null);
                 break;
             case PHONECALL:
-                holder.info.setText("PHONECALL");
+                holder.info.setImageDrawable(null);
                 break;
             case PARTYING:
-                holder.info.setText("PARTYING");
+                holder.info.setImageDrawable(null);
                 break;
             case MEETFRIENDS:
-                holder.info.setText("MEETFRIENDS");
+                holder.info.setImageDrawable(null);
                 break;
             case WATCHTV:
-                holder.info.setText("WATCHTV");
+                holder.info.setImageDrawable(null);
                 break;
             case READINGBOOK:
-                holder.info.setText("READINGBOOK");
+                holder.info.setImageDrawable(null);
                 break;
             case SPORTS:
-                holder.info.setText("SPORTS");
+                holder.info.setImageDrawable(null);
                 break;
             case LEARN:
-                holder.info.setText("LEARN");
+                holder.info.setImageDrawable(null);
                 break;
         }
         holder.activity.setId(position);
@@ -94,7 +96,7 @@ public class PopupAdapter extends RecyclerView.Adapter<PopupAdapter.PopupViewHol
     public class PopupViewHolder extends RecyclerView.ViewHolder {
         public ConstraintLayout activity;
         public TextView title;
-        public TextView info;
+        public ImageView info;
 
         public PopupViewHolder(View view) {
             super(view);
